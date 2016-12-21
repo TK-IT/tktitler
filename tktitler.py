542900f71d52c959fd79770794de17cd57dd9ecb
Mathias Dannesbo <neic@neic.dk>
2016-12-21 15:11:01 +0100

    Rewrite funny_substitude to use multireplace

--- tktitler.py
+++ tktitler.py
@@ -103,9 +103,33 @@ def parse(alias, gfyear=gfyear):
 
 
 def __funny_substitute(root):
-    if root == 'KASS':
-        return 'KA$$'
+    replace_dict = {'KASS': 'KA$$'}
+    root = __multireplace(root, replace_dict)
     return root
+
+
+def __multireplace(string, replacements):
+    """
+    Given a string and a replacement map, it returns the replaced string.
+    :param str string: string to execute replacements on
+    :param dict replacements: replacement dictionary {value to find: value to
+    replace}
+    :rtype: str
+    """
+    # Taken from
+    # https://gist.github.com/bgusach/a967e0587d6e01e889fd1d776c5f3729
+
+    # Place longer ones first to keep shorter substrings from matching where
+    # the longer ones should take place For instance given the replacements
+    # {'ab': 'AB', 'abc': 'ABC'} against the string 'hey abc', it should
+    # produce 'hey ABC' and not 'hey ABc'
+    substrs = sorted(replacements, key=len, reverse=True)
+
+    # Create a big OR regex that matches any of the substrings to replace
+    regexp = re.compile('|'.join(map(re.escape, substrs)))
+
+    # For each match, look up the new string in the replacements
+    return regexp.sub(lambda match: replacements[match.group(0)], string)
 
 def get_period(prefix, postfix, gfyear):
     """
