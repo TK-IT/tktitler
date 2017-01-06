c589de4c7f003f0f769966b85255fda803c0aa9e
Mathias Dannesbo <neic@neic.dk>
2017-01-06 21:23:32 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+base_dir = os.path.dirname(__file__)
+
+setup(
+    name='tktitler',
+    version='0.1.0',
+    description='A library for dealing with TÅGEKAMMER-titles.',
+    long_description=open(os.path.join(base_dir,
+                                       'readme.rst')).read(),
+    url='https://github.com/TK-IT/web',
+    author='TK-IT',
+    licens='Beerware',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(),
+
+    extras_require={
+        'test': ['testfixtures', 'coveralls'],
+    },
+)
