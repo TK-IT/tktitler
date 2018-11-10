493b6b46549a1c444034b825ac978df22696b4b7
Mathias Rav <m@git.strova.dk>
2018-11-10 14:35:43 +0100

    WIP

--- test.py
+++ test.py
@@ -800,4 +800,11 @@ class TestCallable(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    from pyannotate_runtime import collect_types
+    collect_types.init_types_collection()
+    try:
+        with collect_types.collect():
+            unittest.main()
+    finally:
+        collect_types.dump_stats('types.json')
+        print('done')
